8837e15bfe8e442476d49e2cb99d44d06562a381
bkioshn <35752733+bkioshn@users.noreply.github.com>
2019-02-19 20:59:55 +0700

    fix footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,33 +7,25 @@ class Footer extends React.Component {
     render() {
         return(
             <div className="footer">
-                <div className="social">
                     <Row>
-                        <Col xs={{ size: 1, offset: 4 }}>
-                            <div className="icon"><SocialIcon url="http://facebook.com"></SocialIcon></div>
-                        </Col>
-                        <Col xs="1"> 
-                            <div className="icon"><SocialIcon url="http://youtube.com"></SocialIcon></div>
+                        <div className="social">
 
-                        </Col>
-                        <Col xs="1">
-                            <div className="icon"><SocialIcon url="http://twitter.com"></SocialIcon></div>
-                        </Col>
-
-                        <Col xs="1">
-                            <div className="icon"><SocialIcon url="http://instagram.com"></SocialIcon></div>
-                        </Col>
+                            <div className="icon"><SocialIcon bgColor="#ff5a01" fgColor="#FFFFFF" url="http://facebook.com"></SocialIcon></div>
+                            <div className="icon"><SocialIcon bgColor="#ff5a01" fgColor="#FFFFFF" url="http://youtube.com"></SocialIcon></div>
+                            <div className="icon"><SocialIcon bgColor="#ff5a01" fgColor="#FFFFFF" url="http://twitter.com"></SocialIcon></div>
+                            <div className="icon"><SocialIcon bgColor="#ff5a01" fgColor="#FFFFFF" url="http://instagram.com"></SocialIcon></div>
+                        </div>
                     </Row>
                     <Row>
-                        <Col mds={{ size: 1, offset: 5 }}>
-                            <div className="footerMenu">
-                                <p>Warehouse</p>
-                            </div>
-                        </Col>
+                        <div className="footerMenu" >
 
+                        
+                            <a href="#">Warehouse </a>
+                            <a href="#">Store </a>
+                            <a href="#">Pack </a>
+                        </div>
                     </Row>
 
-                </div>
 
                 <Row>
                     <Col>
@@ -41,9 +33,9 @@ class Footer extends React.Component {
                     </Col>
                 </Row>
                 <Row>
-                    <Col><div className="info"><p>Email: Halofullfilment@gmail.com</p></div></Col>
-                    <Col><div className="info"><p>Tel: 02-999-9999</p></div></Col>
-                    <Col><div className="info"><p>Address: Halo Service</p></div></Col>
+                    <Col xs="12" md="4"><div className="info"><p>Email: Halofullfilment@gmail.com</p></div></Col>
+                    <Col xs="12" md="4"><div className="info"><p>Tel: 02-999-9999</p></div></Col>
+                    <Col xs="12" md="4"><div className="info"><p>Address: Halo Service</p></div></Col>
                 </Row>
             </div>
         );
